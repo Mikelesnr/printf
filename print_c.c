1e77427c045e1141feb7eda610f0a7a37f16637f
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-05-18 22:02:44 +0200

    printc - prints string

--- print_c.c
+++ print_c.c
@@ -3,6 +3,28 @@
 #include "main.h"
 
 /**
- *printc - prints
+ *printc - prints string
+ *format - is the first argument
+ *return - returns the number of characters printed
+ */
 
-int printc(char *format, va_list)
+int _printf(const char *format, ...)
+{
+	int size;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	size = _strlen(format);
+	if (size <= 0)
+		return (0);
+
+	va_start(args, format);
+	size = handler(format, args);
+
+	_putchar(-1);
+	va_end(args);
+
+	return (size);
+}
